829b9c6ff142e67f9c939d154fd5f8419dec4d7d
sanjay7900 <singhsanjay790043@gmail.com>
2022-05-24 18:20:08 +0530

    second commit

--- CollectionHandsOn/Program.cs
+++ CollectionHandsOn/Program.cs
@@ -13,7 +13,20 @@ namespace CollectionHandsOn
             CollectionDemo collectionDemo = new CollectionDemo();
             collectionDemo.ArrayConcept();
             Console.WriteLine("--------------------------------------");
-            collectionDemo.IceCreameFlavors();  
+            collectionDemo.IceCreameFlavors();
+            Console.WriteLine("====================================");
+            CountingCharacters countingCharacters = new CountingCharacters();
+            Console.WriteLine();
+            Console.WriteLine("This is with hardCoded String Lorem text:");
+            countingCharacters.startCounting("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("This is with user Input :");
+            countingCharacters.UserPrometStringAndCaseInsesative();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine(" this is with Read A String from a File");
+            countingCharacters.ReadStringfromFile();
             Console.ReadKey();  
         }
     }
